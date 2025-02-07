0fccfdfffd0b18b9f2df82734e2e0dc0f0bda221
Adarshgusain05 <adarshgusain0005@gmail.com>
2025-02-07 14:40:56 +0530

    Add files via upload

---
+++ EXPERIMENT-1/NAME.java
@@ -0,0 +1,7 @@
+public class NAME {
+    public static void main(String[] args){
+        System.out.println("Name: Adarsh Gusain");
+        System.out.println("Address: # Ward -27 , kotdwar,Uttarakhand");
+    }
+    
+}
